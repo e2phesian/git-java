4cd891644bf6aa0320615a7115518fcfd5d023f7
hohoho <hohoho@gmail.com>
2018-05-26 15:20:27 +0900

    c1

--- src/com/ex/hello.java
+++ src/com/ex/hello.java
@@ -2,6 +2,7 @@ package com.ex;
 
 public class hello {
 	public static void main(String[] args) {
-		System.out.println("helloworld");
+		System.out.println("hello");
+		System.out.println("c1 commit");
 	}
 }
